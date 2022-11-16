941a5d953e898fee42c5c030b4513f2d2496b08c
splemoine8 <splemoine8@gmail.com>
2022-11-16 04:31:39 +0000

    Added Netlify URL to config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Second Brain',
+  tagline: 'Because my first one is full.',
+  url: 'https://magenta-cobbler-3682ae.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
