b50eedba5158b397c5806378c591e43d95bca16e
splemoine8 <splemoine8@gmail.com>
2022-11-16 06:13:23 +0000

    Attempted deploy fix v2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,6 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
