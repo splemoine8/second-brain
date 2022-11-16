c8035923cd7779c792de745bf898c75211975a63
splemoine8 <splemoine8@gmail.com>
2022-11-16 05:33:17 +0000

    Testing baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,15 +9,11 @@ const config = {
   title: 'Second Brain',
   tagline: 'Because my first one is full.',
   url: 'https://magenta-cobbler-3682ae.netlify.app/',
-  baseUrl: '/',
+  baseUrl: '/docs/intro/ ',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
